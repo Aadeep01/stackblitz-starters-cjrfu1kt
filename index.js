c4b4c1be0e89f10439e1a2282ab771a59270c88e
Aadeep Kaur <aadeepkaur@yahoo.com>
2025-01-21 10:04:32 +0530

    updated

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-const express = require('express');
-const { resolve } = require('path');
-
-const app = express();
-const port = 3010;
-
-app.use(express.static('static'));
-
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+require('dotenv').config();
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
+module.exports = config;
+console.log(config);
